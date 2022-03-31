34ba4363fd8fa50604fd324e6c7db8c57988cefa
Gupta126 <guptarahul126@gmail.com>
2022-03-31 20:24:52 +0530

    intial commit

---
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.udacity.shoestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Timber.plant(Timber.DebugTree())
+    }
+
+}
